6948feccf67116be04237986eb6db981df0acb84
Bumuthu <bumuthu.dilshan@gmail.com>
2020-05-27 23:46:54 +0530

    ip added

--- main.py
+++ main.py
@@ -11,5 +11,5 @@ api = Api(app)
 api.add_resource(controller.Controller, '/')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=1919, debug=True, use_reloader=True)
+    app.run(host="0.0.0.0", port=80, debug=True, use_reloader=True)
 
