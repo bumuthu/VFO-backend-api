9903c296895a536017574e4bd31ecff96dc5203c
Bumuthu <bumuthu.dilshan@gmail.com>
2020-05-28 20:42:55 +0530

    ip added

--- smplifyx/smplifyx.py
+++ smplifyx/smplifyx.py
@@ -1,6 +1,6 @@
 import base64
 import subprocess
-
+import base64
 import pickle
 
 
@@ -12,24 +12,26 @@ class Smplifyx :
         self.betas = []
         self.recommendation = []
 
+        with open("../scripts/images/img.png", "wb") as fh:
+            fh.write(base64.decodebytes(image))
+
     def get_betas(self):
         self.betas = [0,0,0,0,0,0,0,0]
-        # with open('/home/bumuthudilshanhhk/scripts/make_body_model.sh', 'w') as f:
-        #     f.write('#!/bin/sh\nexit 0')
-        # make_body_model.sh
+
         shellscript = subprocess.Popen(["../scripts/make_body_model.sh"], shell=True,  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, error = shellscript.communicate()
         print('logs...',output)
         print('errors...',error)
 
-        with open('../scripts/output/results/99/000.pkl', 'rb') as f:
+        with open('../scripts/output/results/img/000.pkl', 'rb') as f:
             data = pickle.load(f)
         text = ''
         for i in data:
             text += str(i) + ': ' + str(len(data[i][0])) + '\n'
-        print(text)
-        with open('number_params.txt', 'w') as f:
-            print(text, file=f)
+
+        # print(text)
+        # with open('number_params.txt', 'w') as f:
+        #     print(text, file=f)
 
         shellscript.wait()
         print('oooooooooooooooooooooooo',shellscript.returncode)
