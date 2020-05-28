def3746dba457811272cce0357afeb3b11c09677
Bumuthu <bumuthu.dilshan@gmail.com>
2020-05-28 21:36:43 +0530

    ip added

--- controllers/controller.py
+++ controllers/controller.py
@@ -13,12 +13,11 @@ class Controller(Resource):
 
     def post(self):
         data_in = request.get_json(force=True)
-        print(data_in)
+
         image = data_in['image']
         gender = data_in['gender']
-        print('h1')
         smplifyx_inst = smplifyx.Smplifyx(image, gender)
-        print('h2')
+
         response = {
             'betas':  smplifyx_inst.get_betas(),
             'texture' :  smplifyx_inst.get_texture(),
